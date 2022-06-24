7c0f5c7486f3eca5c6f0b1b0aa1fcd2397244696
VicktzZ <71114564+VicktzZ@users.noreply.github.com>
2022-06-24 13:31:42 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,14 +6,12 @@
     }
 
     body {
-        height: 100vh;
+        min-height: 100vh;
         width: 100vw;
         display: flex;
         justify-content: center;
         align-items: center;
-        font-size: 3vw;
         font-family: consolas;
-        animation: Girar 3s infinite alternate ease-in-out;
     }
 
     main{
@@ -23,17 +21,34 @@
         flex-direction: column;
     }
 
-    
+    h2 {
+        border-bottom: 1px black solid;
+        margin: 10px;
+        text-align: center;
+    }
+
     section{
-        height: 30vh;
-        min-width: 10vw;
+        width: 30vw;
+        height: 50vh;
+        margin: 2vw;
+        border: 1px solid gray;
+        box-shadow: 2px 2px 2px;
+        border-radius: 10px;
+    }
+
+    label {
+        margin: 5px;
     }
 
     form {
         display: flex;
         justify-content: center;
-        align-item: center;
+        align-items: center;
         flex-direction: column;
+    }
+
+    button {
+        margin: 5%;
     }
 </style>
 
@@ -43,12 +58,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contatos</title>
+    <title>Produtos</title>
 </head>
 <body>
     <main>
     <section>
-        <h2>Adicionar Produto:</h2>
+        <h2>Adicionar Produto</h2>
         <form action="add.php" method="post">
             <label for="nome">Nome:</label>
             <input id="nome" name="nome" type="text">
@@ -63,7 +78,7 @@
     </section>
 
     <section>
-        <h2>Alterar Produto:</h2>
+        <h2>Alterar Produto</h2>
         <form action="alt.php" method="post">
             <label for="id">ID:</label>
             <input id="id" name="id" type="text">
@@ -80,7 +95,7 @@
     </section>
 
     <section>
-        <h2>Deletar Produto:</h2>
+        <h2>Deletar Produto</h2>
         <form action="del.php" method="post">
             <label for="id">ID:</label>
             <input id="id" name="id" type="text">
